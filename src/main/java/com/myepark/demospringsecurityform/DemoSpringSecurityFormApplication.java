34f3687b50783797c2459d264203afd7410742c2
myepark <myepark@gmarket.com>
2023-03-12 14:54:40 +0900

    init

---
+++ src/main/java/com/myepark/demospringsecurityform/DemoSpringSecurityFormApplication.java
@@ -0,0 +1,13 @@
+package com.myepark.demospringsecurityform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurityFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringSecurityFormApplication.class, args);
+    }
+
+}
