3b51898f8fedc41c16cd49d4d2e26dc035f8904b
myepark <myepark@gmarket.com>
2023-03-13 23:45:38 +0900

    fix.. .build error...

--- src/main/java/com/myepark/demospringsecurityform/account/AccountService.java
+++ src/main/java/com/myepark/demospringsecurityform/account/AccountService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
